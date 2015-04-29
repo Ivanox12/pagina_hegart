d69ccb3de36a7ff7f01637a1fd858d88020b5fa0
Ivan <ivanox12@hotmail.com>
2015-04-29 12:57:18 -0500

    Add main funcionality and styles

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  
   root 'welcome#homepage'
 
   # Example of regular route:

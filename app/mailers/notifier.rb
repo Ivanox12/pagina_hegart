2087fbde448a743bcfcc100f6d7cf5741d7d1e43
Ivan <ivanox12@hotmail.com>
2015-05-11 00:02:44 -0500

    cambio de correo al de hector

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -9,6 +9,6 @@ class Notifier < ActionMailer::Base
 		if contact_request.phone_number.present?
 			@phone_number = contact_request.phone_number
 		end
-		mail(to: "ivan.quevedo.garcia@gmail.com", subject: "Solicitud de contacto de #{contact_request.name}")
+		mail(to: "hector@hegart.com", subject: "Solicitud de contacto de #{contact_request.name}")
 	end
 end
